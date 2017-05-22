5598b1d07dc46dd606da4a8211534a91212ef6b8
Spencer Gabhart <sgabhart2230@gmail.com>
2017-05-22 12:12:19 -0400

    Restructuring into modules

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,9 @@ use opengl_graphics::GlGraphics;
 use piston::input::*;
 use graphics::{line, grid, color, Transformed};
 
-struct Vec2f {
-    x: f64,
-    y: f64
-}
-
-const CELL_DIMS: f64 =  65.0;
+mod app;
+mod board;
+mod settings;
 
 fn main() {
     let title = "Stones, ma dudes";
@@ -22,10 +19,9 @@ fn main() {
         width: 800,
     };
 
-    let mut mouse_coords = Vec2f{ x: 0.0, y: 0.0 };
-
     let opengl = OpenGL::V2_1;
 
+    let mut app = app::App::new();
     let mut window: PistonWindow =
         WindowSettings::new(title,
                             [window_size.width, window_size.height])
@@ -35,7 +31,7 @@ fn main() {
                             .unwrap();
 
     let line = line::Line::new(color::BLACK, 0.5);
-    let _grid = grid::Grid{ cols: 10, rows: 10, units: CELL_DIMS};
+    let _grid = grid::Grid{ cols: 10, rows: 10, units: settings::CELL_DIMS};
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
@@ -45,39 +41,12 @@ fn main() {
             _grid.draw(&line, &c.draw_state, center.trans(0.0, 0.0), g);
         });
 
-        if let Some(Button::Mouse(button)) = e.press_args() {
-            println!("Pressed mouse button '{:?}'", button);
-            println!("At coordinates {} {}", mouse_coords.x, mouse_coords.y);
-
-            let point = Vec2f{ x:mouse_coords.x, y:mouse_coords.y };
-            let mut row = 0u8;
-            let mut col = 0u8;
-            let mut accumulated_height = 0.0f64;
-            let mut accumulated_width = 0.0f64;
-
-            if point.x > 650.0 || point.y > 650.0 {
-                println!("Located outside grid");
-            }
-            else {
-                for i in 1..10 {
-                    accumulated_width += CELL_DIMS;
-                    if accumulated_width >= point.x { break };
-                    col += 1;
-                }
-
-                for j in 1..10 {
-                    accumulated_height += CELL_DIMS;
-                    if accumulated_height >= point.y { break };
-                    row += 1;
-                }
-
-                println!("Located in cell {}, {}", row, col);
-            }
+        if let Some(button) = e.press_args() {
+            app.on_button_press(&button);
         }
 
         if let Some(args) = e.mouse_cursor_args() {
-            mouse_coords.x = args[0];
-            mouse_coords.y = args[1];
+            app.on_mouse_move(&args);
         }
     }
 }
